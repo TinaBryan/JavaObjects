172348f3d7deb40583c0b703db109888bc0e2243
TinaBryan <tdmak63@gmail.com>
2017-11-30 15:44:12 -0500

    Commiting code challenge solution as reference

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Beginning forum example");
+    if (args.length < 2) {
+      System.out.println("Usage: java Main <first name> <last name>");
+      System.err.println("<first name> and <last name> are required");
+      System.exit(1);
+    }
+   // Uncomment this when prompted
+    Forum forum = new Forum("Java");
+    // TODO: pass in the first name and last name that are in the args parameter
+    User author = new User(args[0], args[1]);
+    // TODO: initialize the forum post with the user created above and a title and description of your choice
+    ForumPost post = new ForumPost(author, " Calorfiniel, Lady of the Galadhrim", "Secrets within the darkness. ");
+    forum.addPost(post);
+  }
+}
